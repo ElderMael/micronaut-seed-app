88bade4c8154381b08e670f3b91dc4908ab787c4
ElderMael <sftwr.mael@gmail.com>
2019-08-09 22:38:05 -0500

    Create Echo Controller

---
+++ src/main/java/seed/micronaut/project/echo/EchoController.java
@@ -0,0 +1,18 @@
+package seed.micronaut.project.echo;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Body;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.Produces;
+
+@Controller("/echo")
+public class EchoController {
+
+  @Post
+  @Produces(MediaType.APPLICATION_JSON)
+  public String echo(@Body String requestBody) {
+    return requestBody;
+  }
+
+}
